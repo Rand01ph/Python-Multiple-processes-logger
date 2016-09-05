0fbbd3540545cc01522a1265997d084787e7647a
Rand01ph <361212419@qq.com>
2016-09-05 14:50:56 +0800

    for 1w log

--- loggenerator.sh
+++ loggenerator.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-cat access.log >> /usr/local/nginx/logs/access.log
-cat pro.log >> /var/log/pro/pro.log
-cat uwsgi.log >> /var/log/uwsgi.log
+for i in {1..100}
+do
+  cat access.log >> /usr/local/nginx/logs/access.log
+  cat pro.log >> /var/log/pro/pro.log
+  cat uwsgi.log >> /var/log/uwsgi.log
+done
